1bc6decf8e79eb2814eeee961a3aed44ed868a93
Abdulahi Abdihakiim <shawkaxe10@gmail.com>
2021-11-20 09:05:48 +0300

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,4 @@
+void main(List<String> arguments) {
+  print('Hello world!');
+  print("Hello this is first code in dart");
+}
---
+++ bin/lesson2.dart
@@ -0,0 +1,5 @@
+void main() {
+  var name = "abdullahi";
+  String name2 = " Ismail";
+  print("My name is ${name + name2}");
+}
